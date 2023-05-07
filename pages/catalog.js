d8b4d6c518eabb69c08095216fb423f234d639c4
myles156 <132836065+myles156@users.noreply.github.com>
2023-05-07 13:17:48 -0400

    Update catalog.js

--- pages/catalog.js
+++ pages/catalog.js
@@ -7,16 +7,16 @@ import navbar from "https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootst
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/Chome">Home/span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="/games">Games</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link" href="/catalog">Catalog</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <a class="nav-link" href="/develop">Develop</a>
       </li>
     </ul>
   </div>
