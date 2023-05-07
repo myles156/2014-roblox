bedd01f2145d50d7152d24af109e5c70b7500783
myles156 <132836065+myles156@users.noreply.github.com>
2023-05-07 13:18:06 -0400

    Update catalog.js

--- pages/catalog.js
+++ pages/catalog.js
@@ -4,7 +4,7 @@ import navbar from "https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootst
 <nav class="navbar navbar-md navbar-light bg-blue">
   <a class="navbar-brand" href="/auth/home"></a>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
+  <div class="navbar-nav" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
         <a class="nav-link" href="/Chome">Home/span></a>
